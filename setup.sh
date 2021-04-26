d8d797e33cadf6f62172c4c29bc12ee16d2f2267
Ales Langer <langerales@yahoo.com>
2021-04-26 15:33:51 +0200

    setup.sh

---
+++ setup.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+ARG=${1:-help}
+
+Help() {
+  echo "build  - Build docker containers"
+  echo "up  - Run docker containers"
+  echo "down  - Stop docker containers"
+  echo "install  - composer install"
+  echo "update  - composer update"
+}
+
+Up() {
+  docker-compose -f docker-compose.yml up -d
+}
+
+Down() {
+  docker-compose -f docker-compose.yml down
+}
+
+Build() {
+  docker-compose -f docker-compose.yml build
+}
+
+Install() {
+  docker-compose -f docker-compose.yml exec php bash -c 'composer install'
+
+}
+
+Update() {
+  docker-compose -f docker-compose.yml exec php bash -c 'composer update'
+
+}
+
+case $ARG in
+help)
+  Help
+  exit
+  ;;
+up)
+  Up
+  exit
+  ;;
+build)
+  Build
+  exit
+  ;;
+down)
+  Down
+  exit
+  ;;
+install)
+  Install
+  exit
+  ;;
+update)
+  Update
+  exit
+  ;;
+esac
